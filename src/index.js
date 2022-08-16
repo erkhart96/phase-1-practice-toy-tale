669b8c72c721f66a8964d0905808fcb76536c61a
Ben <berkhart@rocketmail.com>
2022-08-16 11:06:28 -0500

    Code challenge

--- src/index.js
+++ src/index.js
@@ -13,3 +13,27 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(toys => renderCard(toys));
+
+const toyDiv = document.getElementById('toy-collection');
+const newDiv = document.createElement('div');
+toyDiv.appendChild(newDiv);
+newDiv.className = 'card'
+
+function renderCard(toys) {
+  const h2 = document.createElement('h2')
+  const img = document.createElement('img')
+  const pLikes = document.createElement('p')
+  const btn = document.createElement('button')
+  btn.className = 'like-btn'
+  btn.id = toys.id
+  h2.textContent = toys.name;
+  pLikes.textContent = toys.likes;
+  img.src = toys.image;
+  newDiv.append(h2, img, pLikes, btn)
+}
+
+renderCard(toys);
